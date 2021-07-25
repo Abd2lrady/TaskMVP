114d34ffc57d42ced4532c073227bafb6c2303ee
Ahmad Abdulrady <abdulrady@Ahmads-MacBook-Pro.local>
2021-07-25 17:09:20 +0200

    removing storyboard

--- TaskMVP/ViewController.swift
+++ TaskMVP/ViewController.swift
@@ -12,6 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
     }
-        
 }
